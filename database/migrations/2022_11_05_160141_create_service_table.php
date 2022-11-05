4eddbbec4b7427871612dcc1633c267e768c2058
Syahrul Imron <syahrulimron111@gmail.com>
2022-11-05 23:23:53 +0700

    Updated

---
+++ database/migrations/2022_11_05_160141_create_service_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('service', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('id_user')->index('fk_service_to_users');
+            $table->enum('jenis_service', ['meter_listrik', 'listrik_bangunan']);
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('service');
+    }
+};
---
+++ database/migrations/2022_11_05_161728_add_foreign_keys_jensis_kerusakan_to_service.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('jenis_kerusakan', function (Blueprint $table) {
+            $table->foreign('id_service', 'fk_jenis_kerusakan_to_service')->references('id')->on('service')->onUpdate('cascade')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('jenis_kerusakan', function (Blueprint $table) {
+            $table->dropForeign('fk_jenis_kerusakan_to_service');
+        });
+    }
+};

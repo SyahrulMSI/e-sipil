2a19509061a11a73096b09ad276257deaadfa7ba
Syahrul Imron <syahrulimron111@gmail.com>
2022-11-12 00:49:27 +0700

    Updated Features

--- app/Http/Controllers/Admin/User/Customer/CustomerController.php
+++ app/Http/Controllers/Admin/User/Customer/CustomerController.php
@@ -4,6 +4,9 @@ namespace App\Http\Controllers\Admin\User\Customer;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\USer;
+use Illuminate\Support\Facades\Hash;
+use Alert;
 
 class CustomerController extends Controller
 {
@@ -14,8 +17,11 @@ class CustomerController extends Controller
      */
     public function index()
     {
+        $user = User::orderBy('id', 'DESC')->where('role', 3)->get();
+
         $data = array(
-            'title'     =>  'Data Customer'
+            'title'     =>  'Data Customer',
+            'users'     =>  $user
         );
 
         return view('pages.admin.users.customer.index', $data);
@@ -28,7 +34,11 @@ class CustomerController extends Controller
      */
     public function create()
     {
-        //
+        $data = array(
+            'title'     =>  'Tambah Data Customer',
+        );
+
+        return view('pages.admin.users.customer.add', $data);
     }
 
     /**
@@ -39,7 +49,35 @@ class CustomerController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $request->validate([
+            'nama_lengkap'  =>  'required|min:3|max:100',
+            'email'         =>  'required|unique:users,email',
+            'no_telp'       =>  'nullable|min:11|max:13',
+            'password'      =>  'required|min:8'
+        ]);
+
+        $nama = $request->nama_lengkap;
+        $email  =   $request->email;
+        $no =   $request->no_telp;
+        $password = $request->password;
+
+        $data = array(
+            'nama_lengkap'  =>  $nama,
+            'email' =>  $email,
+            'no_telp'   =>  $no,
+            'password'  =>  Hash::make($password),
+            'role'      =>  3
+        );
+
+        $result = User::create($data);
+
+        if($result){
+            Alert::success('Success', 'Data berhasil disimpan.');
+            return redirect()->route('admin.customer.index');
+        } else {
+            Alert::error('Error', 'Data gagal disimpan.');
+            return redirect()->route('admin.customer.create');
+        }
     }
 
     /**
@@ -61,7 +99,14 @@ class CustomerController extends Controller
      */
     public function edit($id)
     {
-        //
+        $user = User::where('id', $id)->first();
+
+        $data = array(
+            'title'     =>  'Edit Data Customer',
+            'user'      =>  $user
+        );
+
+        return view('pages.admin.users.customer.edit', $data);
     }
 
     /**
@@ -73,7 +118,57 @@ class CustomerController extends Controller
      */
     public function update(Request $request, $id)
     {
-        //
+        $request->validate([
+            'nama_lengkap'  =>  'required|min:3|max:100',
+            'email'         =>  'required|unique:users,email,' . $id,
+            'no_telp'       =>  'nullable|min:11|max:13',
+            'password'      =>  'nullable|min:8'
+        ]);
+
+        $nama = $request->nama_lengkap;
+        $email  =   $request->email;
+        $no =   $request->no_telp;
+        $password = $request->password;
+
+        if($request->password == null){
+
+            $data = array(
+                'nama_lengkap'  =>  $nama,
+                'email' =>  $email,
+                'no_telp'   =>  $no,
+                'role'      =>  3
+            );
+
+            $result = User::where('id', $id)->update($data);
+
+            if($result){
+                Alert::success('Success', 'Data berhasil diupdate.');
+                return redirect()->route('admin.customer.index');
+            } else {
+                Alert::error('Error', 'Data gagal diupdate.');
+                return redirect()->route('admin.customer.create');
+            }
+
+        } else {
+
+            $data = array(
+                'nama_lengkap'  =>  $nama,
+                'email' =>  $email,
+                'no_telp'   =>  $no,
+                'password'  =>  Hash::make($password),
+                'role'      =>  3
+            );
+
+            $result = User::where('id', $id)->update($data);
+
+            if($result){
+                Alert::success('Success', 'Data berhasil diupdate.');
+                return redirect()->route('admin.customer.index');
+            } else {
+                Alert::error('Error', 'Data gagal diupdate.');
+                return redirect()->route('admin.customer.edit', $id);
+            }
+        }
     }
 
     /**
@@ -84,6 +179,14 @@ class CustomerController extends Controller
      */
     public function destroy($id)
     {
-        //
+        $result = User::destroy($id);
+
+        if($result){
+            Alert::success('Success', 'Data berhasil dihapus.');
+            return redirect()->route('admin.customer.index');
+        } else {
+            Alert::error('Error', 'Data gagal dihapus.');
+            return redirect()->route('admin.customer.index');
+        }
     }
 }
--- resources/views/pages/admin/users/admin/index.blade.php
+++ resources/views/pages/admin/users/admin/index.blade.php
@@ -55,7 +55,7 @@
                                                     <div class="modal-header bg-danger">
                                                       <h5 class="modal-title text-white font-weight-bold" id="staticBackdropLabel">Konfirmasi Hapus Data</h5>
                                                       <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                                                        <span aria-hidden="true">&times;</span>
+                                                        <span ar    ia-hidden="true">&times;</span>
                                                       </button>
                                                     </div>
                                                     <div class="modal-body">
